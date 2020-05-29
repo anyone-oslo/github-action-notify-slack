68a595b6f40697610c94b709de6a82e3884f00f1
Inge Jørgensen <inge@anyone.no>
2020-05-30 01:11:20 +0200

    Finish script

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-action-notify-slack
+
+Adapted from: https://gist.github.com/trung/b77e0455f9bc1b39e8a530538b6daf1d
--- index.js
+++ index.js
@@ -2,12 +2,123 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const slackWebhookUrl = core.getInput('slack-webhook-url');
-  console.log(`Webhook URL ${slackWebhookUrl}!`);
+  // const slackWebhookUrl = core.getInput('slack-webhook-url');
+  // console.log(`Webhook URL ${slackWebhookUrl}!`);
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // console.log(`The event payload: ${payload}`);
+
+  const token = core.getInput('token');
+  const runId = core.getInput('run-id');
+  const runNumber = core.getInput('run-number');
+  const octokit = new github.GitHub(token);
+
+  const dateDiff = function(start, end) {
+    var duration = end - start;
+    // format the duration
+    var delta = duration / 1000;
+    var days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+    var hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+    var minutes = Math.floor(delta / 60) % 60;
+    delta -= minutes * 60;
+    var seconds = Math.floor(delta % 60);
+    var format_func = function(v, text, check) {
+      if (v <= 0 && check) {
+        return "";
+      } else {
+        return v + text;
+      }
+    };
+    return format_func(days, "d", true) +
+      format_func(hours, "h", true) +
+      format_func(minutes, "m", true) +
+      format_func(seconds, "s", false);
+  };
+
+  const statusIcon = function(s) {
+    switch (s) {
+    case "w_success":
+      return ":white_check_mark:";
+    case "w_failure":
+      return ":no_entry:";
+    case "w_cancelled":
+      return ":warning:";
+    case "success":
+      return "\u2713";
+    case "failure":
+      return "\u2717";
+    default:
+      return "\u20e0";
+    }
+  };
+
+  octokit.actions.getWorkflowRun({
+    owner: github.context.payload.repository.organization,
+    repo: github.context.payload.repository.name,
+    run_id: runId
+  }).then((wfRun) => {
+    octokit.request(wfRun.data.jobs_url).then((jobs) => {
+      const commit = github.context.sha.substr(0, 6);
+      let wfStatus = "w_cancelled";
+
+      let pr = "";
+      for (p of wfRun.data.pull_requests) {
+        pr += ",<" + p.url + "|#" + p.number + ">";
+      }
+      if (pr != "") {
+        pr = "for " + pr.substr(1);
+      }
+
+      // build the message
+      let fields = [];
+      var wfSucceeded = true;
+      var wfFailed = false;
+      for (j of jobs.data.jobs) {
+        // ignore the current job running this script
+        if (j.status != "completed") {
+          continue;
+        }
+        if (j.conclusion != "success") {
+          wfSucceeded = false;
+        }
+        if (j.conclusion == "failure") {
+          wfFailed = true;
+        }
+        fields.push({
+          type: "mrkdwn",
+          text: statusIcon(j.conclusion) + " <" + j.html_url + "|" + j.name + ">\n  \u21b3 completed in " + dateDiff(new Date(j.started_at), new Date(j.completed_at))
+        });
+      }
+      if (wfSucceeded) {
+        wfStatus = "w_success";
+      } else if (wfFailed) {
+        wfStatus = "w_failure";
+      }
+
+
+      core.setOutput("message", {
+        blocks: [
+          { type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `<${github.context.payload.repository.url}|*${github.context.payload.repository.full_name}*>\nfrom *${github.context.ref}@` +
+                commit + "*"
+          }
+        },
+        { type: "section",
+          text: { type: "mrkdwn",
+                  text: statusIcon(wfStatus) + ` *${github.context.workflow}* ` + pr +
+                  "\nWorkflow run <" + wfRun.data.html_url +  `|#${runNumber}> completed in ` +
+                  dateDiff(new Date(wfRun.data.created_at), new Date(wfRun.data.updated_at)) }
+        },
+        { type: "divider" },
+        { type: "section", fields: fields }
+      ]});
+    });
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
