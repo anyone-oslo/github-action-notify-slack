bd90da9a78d7c4fa09032b84757b3ed0e0f17c16
Inge Jørgensen <inge@anyone.no>
2023-11-03 16:50:58 +0100

    Update to yarn

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  overrides: [
+    {
+      files: ["**/*.+(ts|tsx)"],
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+      ]
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: true,
+    sourceType: "module",
+    tsconfigRootDir: __dirname
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: ["error", "always"]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
